076cf1b0ccf873764c2277c1a8f86e4dccf30703
yohan.an <an.sangkil@gmail.com>
2024-07-18 11:25:16 +0900

    refactoring :  empty remove

--- tempo-zipkinlibrary-project/ad-server/src/main/kotlin/com/ad/adserver/AdController.kt
+++ tempo-zipkinlibrary-project/ad-server/src/main/kotlin/com/ad/adserver/AdController.kt
@@ -1,10 +1,15 @@
 package com.ad.adserver
 
+import lombok.extern.slf4j.Slf4j
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.awaitExchange
+import org.springframework.web.reactive.function.client.bodyToMono
 
 
+@Slf4j
 @RestController
 class AdController {
 
@@ -13,9 +18,15 @@ class AdController {
     fun recommendAd(@RequestParam(required = false) age:String):AdRecommend{
 
 
-        val recommend = AdRecommend("","","","");
+//       WebClient.create()
+//           .get()
+//           .uri("http://naver.com")
+//           .exchangeToMono {resposne-> resposne.bodyToMono<String>()}
+//           .block()
 
-        return recommend;
+        return AdRecommend("","","","")
+
+
     }
 }
 
