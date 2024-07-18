b3d9652da806c691b965e5eef66667f0fcbcc1aa
yohan.an <an.sangkil@gmail.com>
2024-07-18 11:26:35 +0900

    init : save module add

---
+++ tempo-zipkinlibrary-project/ad-oltp-zipkin-repository/src/main/java/com/kakaohealthcare/adoltpzipkinrepository/AdOltpZipkinRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.kakaohealthcare.adoltpzipkinrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdOltpZipkinRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdOltpZipkinRepositoryApplication.class, args);
+	}
+
+}
---
+++ tempo-zipkinlibrary-project/ad-oltp-zipkin-repository/src/main/java/com/kakaohealthcare/adoltpzipkinrepository/AdRepositoryController.java
@@ -0,0 +1,53 @@
+package com.kakaohealthcare.adoltpzipkinrepository;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
+
+/**
+ * Description
+ *
+ * @author yohan.an
+ * @version Copyright (C) 2024 by KakaoHealthcare. All right reserved.
+ * @since 2024. 7. 17.
+ */
+@Slf4j
+@RestController
+public class AdRepositoryController {
+
+    @PostMapping("/ad/save")
+    public Mono<String> adSave(ServerHttpRequest serverRequest, Campaign campaign) {
+
+
+        log.info("=======================Ad Repository Save Request Headers==========================");
+        serverRequest.getHeaders().forEach((s, strings) -> {
+            log.info("key {} value {}", s, strings);
+        });
+
+        log.info("path{} , request body {}", "/ad/save", campaign);
+
+
+        return Mono.just("ad save...");
+    }
+
+    @DeleteMapping("/ad/delete")
+    public Mono<String> adDelete(ServerHttpRequest serverRequest) {
+
+
+        log.info("=======================Ad Repository Delete Request Headers==========================");
+        serverRequest.getHeaders().forEach((s, strings) -> {
+            log.info("key {} value {}", s, strings);
+        });
+
+        log.info("path{} , request body {}", "/ad/delete");
+
+
+        return Mono.just("ad delete...");
+    }
+}
