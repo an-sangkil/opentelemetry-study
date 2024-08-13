ad4e276fc60d1154a3737f887d987821d54ba4a2
yohan.an <an.sangkil@gmail.com>
2024-08-13 20:28:36 +0900

    fix : service 폴더 이동

--- tempo-zipkinlibrary-project/ad-oltp-zipkin-create/src/main/java/com/ad/adrepository/service/AdCreateService.java
+++ tempo-zipkinlibrary-project/ad-oltp-zipkin-create/src/main/java/com/ad/adrepository/service/AdCreateService.java
@@ -1,6 +1,7 @@
-package com.ad.adrepository.controller;
+package com.ad.adrepository.service;
 
 import com.ad.adrepository.config.Traced;
+import com.ad.adrepository.controller.Campaign;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
