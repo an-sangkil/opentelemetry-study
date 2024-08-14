d2899a64ef029918fbe194eda8690d5e2e7a0ff3
yohan.an <an.sangkil@gmail.com>
2024-08-14 22:34:21 +0900

    fix : 오픈텔레메 트리전파전략 추가  b3, w3c

---
+++ tempo-zipkinlibrary-project/ad-oltp-zipkin-create/src/main/java/com/ad/adrepository/controller/MapTextMapGetter.java
@@ -0,0 +1,9 @@
+package com.ad.adrepository.controller;/**
+ * Description 
+ *
+ *
+ * @author yohan.an
+ * @version Copyright (C) 2024 by KakaoHealthcare. All right reserved.
+ * @since 2024. 8. 13.
+*/public class AAA {
+}
