aae40571e7ebffc463483d54fdd9558359487450
skan-win <mycup@nate.com>
2025-07-31 21:50:05 +0900

    모듈 추가

---
+++ otel-collector-project/collector-api-gateway/src/main/java/org/example/collectorapigateway/CollectorApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.example.collectorapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectorApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectorApiGatewayApplication.class, args);
+    }
+
+}
---
+++ otel-collector-project/collector-api-gateway/src/test/java/org/example/collectorapigateway/CollectorApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.collectorapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectorApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
