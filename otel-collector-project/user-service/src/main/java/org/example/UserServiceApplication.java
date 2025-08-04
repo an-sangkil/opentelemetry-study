1612c92e30041d724c821703607efa05d5fab724
yohan.an <an.sangkil@gmail.com>
2025-08-04 13:28:30 +0900

    add: user service 추가

---
+++ otel-collector-project/user-service/src/main/java/org/example/UserServiceApplication.java
@@ -0,0 +1,15 @@
+package org.example;
+
+/**
+ * Description
+ *
+ * @author yohan.an
+ * @version Copyright (C) 2025 by KakaoHealthcare. All right reserved.
+ * @since 2025. 8. 4.
+ *///TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class UserServiceApplication {
+    public static void main(String[] args) {
+
+    }
+}
